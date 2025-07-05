b2909a1343c26339d93294c2200ea973dedd7afe
Gene Boggs <gene@ology.net>
2025-07-04 20:50:35 -0700

    WIP

--- Shopping.pas
+++ Shopping.pas
@@ -216,16 +216,21 @@ begin
   if TabControl1.TabIndex = 0 then
   begin
     if MessageDlg('Delete item id ' + GetFieldValue(0) + '?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+    try
     begin
       FDQuery11.ParamByName('id').AsInteger := StrToInt(GetFieldValue(0));
       FDQuery11.ExecSQL;
       FDQuery13.Close;
       FDQuery13.Open;
-    end
+    end;
+    except
+      ShowMessage('Cannot delete item');
+    end;
   end
   else
   begin
     if MessageDlg('Remove item id ' + GetFieldValue(0) + ' from store?', mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+    try
     begin
       FDQuery10.ParamByName('store').AsInteger := 0;
       FDQuery10.ParamByName('item').AsInteger := StrToInt(GetFieldValue(0));
@@ -233,7 +238,10 @@ begin
       FDQuery6.Close;
       FDQuery6.ParamByName('store').AsString := TabControl1.Tabs[TabControl1.TabIndex];
       FDQuery6.Open;
-    end
+    end;
+    except
+      ShowMessage('Cannot delete item');
+    end;
   end;
 end;
 
